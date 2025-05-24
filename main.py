1afd0f96bccde63ad8522152aca658d3b9915d0e
nwidmaier <nwidmaier@gmail.com>
2025-05-24 13:33:05 -0700

    added circleshape.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")  # type: ignore
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # type: ignore
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,6 +19,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = game_clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
